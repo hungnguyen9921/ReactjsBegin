6751b4bfe8a7b6f010bdbd110116c353065f8f94
hungnguyen9921 <hungvtvt01@gmail.com>
2022-09-17 23:37:18 +0700

    update

--- client/src/components/Image/Image.js
+++ client/src/components/Image/Image.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+
+export default function Image(({src, alt,...props})) {
+      return (
+            <img
+                  className={classNames(className)}
+                  ref={ref}
+                  src={fallback || src}
+                  alt={alt}
+                  {...props}
+                  onError={handleError}
+            />
+      );
+}
+
+Image.propTypes = {
+      src: PropTypes.string,
+      alt: PropTypes.string,
+      className: PropTypes.string,
+      fallback: PropTypes.string,
+};
